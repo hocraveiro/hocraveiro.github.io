aeb4640274268a7037c633cc1a98867f939e832b
Henrique Craveiro <hocraveiro@gmail.com>
2018-07-09 11:04:35 -0300

    compress images

--- dist/path----0c3e97f9d87576f113dd.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{105:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Henrique Craveiro"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----0c3e97f9d87576f113dd.js.map
--- dist/path---about-63afc65e5f6904a92a88.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xf927f8900006],{335:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Henrique Craveiro"}}},pathContext:{}}}});
-//# sourceMappingURL=path---about-63afc65e5f6904a92a88.js.map
